032558e3e006d69952e855a77fe98018579e734d
jokerkras <krasmax97@mail.ru>
2019-08-23 16:49:01 +0300

    add Mem Card UI

---
+++ app/src/main/java/com/example/krasikovsurfeducation/view/intr/MemListView.kt
@@ -0,0 +1,10 @@
+package com.example.krasikovsurfeducation.view.intr
+
+import moxy.MvpView
+
+interface MemListView: MvpView {
+    fun refreshList()
+    fun addMemToFavorite()
+    fun removeMemFromFavorite()
+    fun shareMem()
+}
